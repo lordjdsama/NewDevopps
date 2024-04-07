a35f6afbaf4e20ad41ef07d048509fd9752e8733
EC2 Default User <ec2-user@ip-172-31-40-105.eu-west-1.compute.internal>
2024-04-07 11:23:29 +0000

    second commit

--- config/settings.py
+++ config/settings.py
@@ -122,6 +122,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/lessons/static'
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
 
 LOGIN_REDIRECT_URL = 'lessons-home'
 LOGIN_URL = 'users-login'
